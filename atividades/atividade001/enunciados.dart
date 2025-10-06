cac1642bd85780ddf8c06e3b773ace42f42d0984
Brendon João <brendonjoaodev@gmail.com>
2025-10-05 21:19:27 -0300

    Atividade001: Enunciados

---
+++ atividades/atividade001/enunciados.dart
@@ -0,0 +1,19 @@
+// A - Faça um programa que mostre a mensagem "Olá Mundo!" na tela.
+// B - Faça um programa que peça 2 valores. Calcule e imprima  a soma, o produto, a subtração, a divisão, o resto da divisão e a divisão inteira.
+// C - Faça um programa que peça um ano qualquer. O programa deverá calcular e imprimir a idade.
+// D - Faça um programa que peça 4 notas com  entrada de dados. O programa deverá calcular a média das notas digitadas.
+// E - Faça um programa que receba e divida 2 números. A saída da divisão precisará ser formatada com 4 casas decimais.
+// F - Faça um programa que recebe um número inteiro e mostre o sucessor e antecessor.
+// G - Faça um programa que receba um número qualquer e calcule o dobro e o triplo desse número.
+// H - Faça um programa para converter medidas
+// I - Faça um programa que receba um número inteiro. O programa deverá imprimir tabuada de multiplicação deste número.
+// J - Faça um programa que receba um valor em reais, depois calcule quantos euros, dólares e wons dariam para comprar com esse valor.
+// K - Faça um programa com entrada de dados para calcular a área de 4 objetos.
+// L - Faça um programa para receber um número qualquer e informar na tela se é par ou ímpar. 
+// M - Faça um algoritmo que leia dois valores inteiros A e B. Se os valores forem iguais deverá somar os dois, caso contrário multiplique A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado para uma variável C e mostrar seu conteúdo na tela. 
+// N - Faça um programa para encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado. 
+// O - Faça um programa que leia uma variável e some 5 caso seja par ou some 8 caso seja ímpar, imprimir o resultado desta operação. 
+// P - Faça um programa que leia três valores inteiros e diferentes e mostre-os em ordem decrescente. 
+// Q - Faça um programa para calcular o IMC COMPLETO de uma pessoa.
+// R - Faça um programa para conversão de temperaturas (C => F  e F ⇒ C)
+// S - Faça um programa para calcular juros.
