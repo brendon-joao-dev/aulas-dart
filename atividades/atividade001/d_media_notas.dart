311251894ea3f3e5faf4a9658197e01a6e59204b
Brendon João <brendonjoaodev@gmail.com>
2025-10-13 16:10:02 -0300

    Atividade001: D - Calculadora de Média de Notas - Validações

--- atividades/atividade001/d_media_notas.dart
+++ atividades/atividade001/d_media_notas.dart
@@ -1,22 +1,48 @@
 import "dart:io";
 
-int soma = 0;
+double soma = 0.0;
 
 void main () {
   print("~"*30);
   print("Calculadora de média de notas");
   print("~"*30);
-  for (int i = 1; i <= 4; i++) {
-    stdout.write("Digite a $iª nota: ");
-    String nota = stdin.readLineSync()!;
+
+  while (true) {
+    // Looping para 4 entradas
+    for (int i = 1; i <= 4; i++) {
+      stdout.write("Digite a $iª nota: ");
+      String entrada = stdin.readLineSync()!;
+      print("~"*30);
+
+      // Valida cada entrada sendo ela um número
+      if (double.tryParse(entrada) != null && entrada.isNotEmpty) {
+        double nota = double.tryParse(entrada)!;
+        soma += nota;
+      // Se não for um número
+      } else {
+        print("Por favor digite um nota numérica!");
+        print("~"*30);
+
+        // Mantém o looping na mesma volta
+        i--;
+        continue;
+      }
+    }
+
+    // Cálcula a média
+    double media = soma / 4;
+
+    print("A média das notas inseridas é: $media");
     print("~"*30);
-    
-    soma += int.parse(nota);
+
+    stdout.write("Pressione enter para calcular outra média ou 0 para sair: ");
+    String continuar = stdin.readLineSync()!;
+    print("~"*30);
+
+    if (continuar == "0") {
+      break;
+    } else {
+      continue;
+    }
   }
-
-  double media = soma / 4;
-
-  print("~"*30);
-  print("A média das notas inseridas é: $media");
-  print("~"*30);
 }
