6ab07c11ef37429e6961cd04740b7644e1f6556f
Brendon João <brendonjoaodev@gmail.com>
2025-10-13 01:44:02 -0300

    Atividade001: A - Hello World! - Comentários

--- atividades/atividade001/a_hello_world.dart
+++ atividades/atividade001/a_hello_world.dart
@@ -1,3 +1,4 @@
+// Importação de bibliotecas
 import "dart:io";
 
 void main() {
@@ -7,21 +8,29 @@ void main() {
 
   stdout.write("Pressione enter para escrever outra mensagem ");
   stdin.readLineSync();
+  print("~"*30);
 
   while (true) {
     stdout.write("Digite a nova mensagem: ");
     var mensagem = stdin.readLineSync();
 
     if (mensagem != null && mensagem.isNotEmpty) {
+      print("~"*30);
       print(mensagem);
+      print("~"*30);
       stdout.write("Pressione enter para escrever outra mensagem ou 0 para sair: ");
       var continuar = stdin.readLineSync();
+      print("~"*30);
 
       if (continuar == "0") {
         break;
       } else {
         continue;
       }
+    } else {
+      print("~"*30);
+      print("Por favor digite uma mensagem!");
+      print("~"*30);
     }
   }
 }
