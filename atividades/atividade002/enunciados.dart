1bd8ce8f4003cd5f6b36df263b51e86f25f65a05
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-10-21 21:41:13 -0300

    Atividade002: Enunciados

---
+++ atividades/atividade002/enunciados.dart
@@ -0,0 +1,15 @@
+// A - Números primos no intervalo: Faça um programa que receba dois números inteiros A e B e imprima todos os números primos entre A e B (inclusive).
+// B - Fatorial com repetição: Faça um programa que leia um número inteiro positivo e calcule seu fatorial usando um loop.
+// C - Soma dos múltiplos: Faça um programa que receba um número inteiro N e calcule a soma de todos os múltiplos de 3 ou 5 menores que N.
+// D - Verificação de ano bissexto: Faça um programa que receba um ano e verifique se ele é bissexto, exibindo a resposta.
+// E - Tabela de velocidades: Faça um programa que leia o tempo (em segundos) e a aceleração constante de um objeto e imprima sua velocidade a cada segundo até o tempo informado.
+// F - Conversão de energia: Faça um programa que leia um valor em joules e converta para calorias (1 caloria = 4,184 joules), exibindo os resultados.
+// G - Sequência de potências:Faça um programa que receba um número inteiro N e imprima as potências de 2 de 0 até N (2⁰, 2¹, ..., 2ᴺ).
+// H - Cálculo de velocidade média: Faça um programa que receba as distâncias percorridas a cada intervalo de tempo e calcule a velocidade média total do percurso.
+// I - Classificação de ângulos: Faça um programa que receba um ângulo em graus e classifique-o como agudo (<90°), reto (90°), obtuso (>90° e <180°) ou rente (180°).
+// J - Contagem regressiva: Faça um programa que receba um número inteiro positivo e faça uma contagem regressiva até zero, imprimindo cada número.
+// K - Determinação do maior divisor comum (MDC): Faça um programa que receba dois números inteiros e calcule o MDC usando o algoritmo de Euclides com repetição.
+// L - Resfriamento de um líquido: Faça um programa que simule o resfriamento de um líquido a partir de uma temperatura inicial até atingir a temperatura ambiente (25°C). A cada segundo, a temperatura diminui 2%. Imprima a temperatura a cada segundo.
+
+// Obrigatório: 
+// -  Elementos obrigatórios: Entrada de dados;  Tratamento Nullsafety ; Validação de Nulo;  Estrutura de Repetição While; Interação do usuário para continuar ou terminar o programa.
