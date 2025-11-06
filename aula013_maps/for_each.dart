c39a2abc96795683774ef9c0e0fbb98494b45017
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-11-05 21:38:00 -0300

    Aula013: Varredura de Maps com For Each - Início

---
+++ aula013_maps/for_each.dart
@@ -0,0 +1,7 @@
+void main() {
+  // Criando um Map aninhado:
+  // - O Map principal tem com chave o NOME da atriz (tipo String).
+  // - O valor associado a cada chave é outro Map (submapa).
+  // - Esse submapa contém duas informações sobre a atriz:
+  
+}
