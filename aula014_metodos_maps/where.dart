43f26ae62dd2ea19a78bf9c0501b90b00c78d9ef
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-11-10 20:58:50 -0300

    Aula014: Métodos para Maps - where()

--- aula014_metodos_maps/where.dart
+++ aula014_metodos_maps/where.dart
@@ -1,14 +1,14 @@
 void main() {
-  print("-"*70);
+  print("-" * 70);
   print("EXEMPLO DE WHERE EM MAPA");
-  print("="*70);
+  print("=" * 70);
 
   Map<String, int> pessoas = {
     "Alice": 20,
     "Bob": 17,
     "Carol": 25,
     "Dave": 16,
-    "Eve": 30
+    "Eve": 30,
   };
 
   // Filtra apenas as pessoas com idade maior que 18 anos
@@ -16,10 +16,12 @@ void main() {
   // 2. where((element) => element.value > 18) filtra apenas as entradas cujo valor (idade) é maior que 18
   // 3. Map.fromEntries(...) converte novamente o Iterable filtrado em um Map
 
-  Map<String, int> pessoasMaiores = Map.fromEntries(pessoas.entries.where((element) => element.value > 18));
+  Map<String, int> pessoasMaiores = Map.fromEntries(
+    pessoas.entries.where((element) => element.value > 18),
+  );
 
   // Saída esperada:
   print("Pessoas: $pessoas");
   print("Maiores de idade: $pessoasMaiores"); // {Alice: 20, Carol: 25, Eve: 30}
-  print("-"*70);
+  print("-" * 70);
 }
