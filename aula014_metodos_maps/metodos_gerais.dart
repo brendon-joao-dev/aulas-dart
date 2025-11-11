d08e247c63ddc4ecd9fa10cb843fffe79a601505
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-11-10 21:41:34 -0300

    Aula014: Métodos para Maps - Métodos gerais

---
+++ aula014_metodos_maps/metodos_gerais.dart
@@ -0,0 +1,50 @@
+void main() {
+  // length -> Retorna o total de pares chave/valor no Map
+  Map<String, int> mapLength = {"a": 1, "b": 2, "c": 3};
+  print("Map em questão: $mapLength");
+  print("Este Map tem tamanho: ${mapLength.length}");
+  print("-"*70);
+
+  // isEmpty / isNotEmpty -> Verificam se o Map está vazio ou não
+  dynamic mapTesteVazio = {};
+  dynamic mapTesteCheio = {"a": 1};
+
+  print("Maps em questão:");
+  print("Vazio: $mapTesteVazio");
+  print("Map vazio? ${mapTesteVazio.isEmpty}"); // true
+  print("Map não vazio? ${mapTesteVazio.isNotEmpty}"); // false
+
+  print("Cheio: $mapTesteCheio");
+  print("Map cheio? ${mapTesteCheio.isEmpty}"); // false
+  print("Map não cheio? ${mapTesteCheio.isNotEmpty}"); // true
+  print("-"*70);
+
+  // keys / values -> Retornam todas as chaves e valores (Iterable)
+  Map<String, int> mapChaveValor = {"a": 1, "b": 2, "c": 3};
+  print("Map em questão: $mapChaveValor");
+  print("Chaves: ${mapChaveValor.keys}"); // (a, b, c)
+  print("Valores: ${mapChaveValor.values}"); // (1, 2, 3)
+  print("-"*70);
+
+  // containsKey / containsValue -> Verificam a existÊncia de chaves e valores
+  Map<String, int> mapContain = {"a": 1, "b": 2, "c": 3};
+
+  print("Map em questão: $mapContain");
+  print("O Map contém a chave 'a'? ${mapContain.containsKey("a")}"); // true
+  print("O Map contém a chave 'a'? ${mapContain.containsKey("d")}"); // false
+
+  print("O Map contém o valor 1? ${mapContain.containsValue(1)}"); // true
+  print("O Map contém o valor 4? ${mapContain.containsValue(4)}"); // false
+  print("-"*70);
+
+  // putIfAbsent -> adiciona uma chave/valor apenas se a chave não existir
+  Map<String, int> testeAbsent = {"a": 1, "b": 2, "c": 3};
+  print("Map em questão: $testeAbsent");
+  print("Adicionando o par chave/valor d:4");
+  testeAbsent.putIfAbsent("d", () => 4);
+  print("Map após processo: $testeAbsent"); // {a: 1, b: 2, c: 3, d: 4}
+
+  print("Map em questão: $");
+  print("");
+  print("-"*70);
+}
